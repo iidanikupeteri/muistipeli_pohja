8bcf1f7c416b4ce0f19e6cf438b85e20809132a8
iidanikupeteri <iida.nikupeteri@gmail.com>
2017-02-28 16:05:34 +0200

    v1

---
+++ js/muistipeli.js
@@ -0,0 +1,41 @@
+var kortit=new Array('sininen','sininen','punainen','punainen','vihrea','vihrea', 'oranssi','oranssi', 'violetti','violetti', 'ruskea','ruskea');
+var ekaKortti='';
+
+$(document).ready(function(){
+	$('#aloita').click(function(e) {
+		$('#pelialue').empty();
+		$.each(kortit, function(key,value) {
+			$('#pelialue').append("<div id='" + key + "' class='" + value + 
+				" kortti' style='background-image: url(img/tausta.png)';></div>");
+		});
+	});
+	$('#sisalto').delegate('.kortti', 'click', function() {
+	if ($(this).attr('style')!==undefined) {
+		$(this).removeAttr('style');	
+		if (ekaKortti==='') {
+			ekaKortti=$(this).attr('id');
+		}
+		else {
+			if ($(this).attr('id')!==ekaKortti) {
+				$('#arvauksia').text(++arvauksia);
+				if ($(this).hasClass($('#' + ekaKortti).attr('class'))) {
+					ekaKortti='';
+				}
+				else {
+					$(this).animate({
+						backgroundImage: 'url(img/tausta.png)'
+					},500,function() {
+						$('#' + ekaKortti).css('background-image', 'url(img/tausta.png)');
+						$(this).css('background-image', 'url(img/tausta.png)');
+						ekaKortti='';
+					});
+				}
+			}
+		}
+	}
+});
+
+
+
+});
+
